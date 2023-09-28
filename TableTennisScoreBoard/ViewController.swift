5d9353a4d1d13c79076774b347ae8d00489361c9
Howe <howewu1991@gmail.com>
2023-09-28 19:38:51 +0800

    UI & AutoLayout 完成

--- TableTennisScoreBoard/ViewController.swift
+++ TableTennisScoreBoard/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var player1Score = 0
+    var player1RoundScore = 0
+    var player2Score = 0
+    var player2RoundScore = 0
+    
+    
+    @IBOutlet weak var player1RoundScoreLabel: UILabel!
+    @IBOutlet weak var player1RoundScoreButtonOutlet: UIButton!
+    @IBOutlet weak var player1NameTextfield: UITextField!
+    @IBOutlet weak var player1ScoreLabel: UILabel!
+    @IBOutlet weak var player1ScoreButtonOutlet: UIButton!
+    
+    @IBOutlet weak var leftServeLabel: UILabel!
+    
+    
+    @IBOutlet weak var player2RoundScoreLabel: UILabel!
+    @IBOutlet weak var player2RoundScoreButtonOutlet: UIButton!
+    @IBOutlet weak var player2NameTextfield: UITextField!
+    @IBOutlet weak var player2ScoreLabel: UILabel!
+    @IBOutlet weak var player2ScoreButtonOutlet: UIButton!
+    
+    @IBOutlet weak var rightServeLabel: UILabel!
+    
+    
+    @IBOutlet weak var duceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
+        
+        
+        
     }
-
-
+    // MARK: - viewDidLoad Over
+    
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
